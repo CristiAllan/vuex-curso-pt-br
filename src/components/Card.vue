3372d7e459feaabd000d822c53951f92736b81c4
CristiAllan <cristiallan@gmail.com>
2019-10-13 16:30:38 -0300

    setup project template part 2

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    width="500"
+    height="600"
+  >
+
+    <v-card-title>{{ cardTitle }}</v-card-title>
+
+    <v-card-text>{{ firstText }}</v-card-text>
+    <v-card-text>{{ secondText }}</v-card-text>
+
+    <slot></slot>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'cardTitle',
+    'firstText',
+    'secondText'
+  ],
+  data: () => ({
+    //
+  })
+}
+</script>
