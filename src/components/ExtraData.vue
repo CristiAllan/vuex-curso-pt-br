05ed0b718033f7ceaaba9004e277deaf4ddaf321
CristiAllan <cristiallan@gmail.com>
2019-10-13 16:30:50 -0300

    setup project template part 3

---
+++ src/components/ExtraData.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="extra-data">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
+
+<style scoped>
+  .extra-data {
+    margin: 15px;
+  }
+</style>
