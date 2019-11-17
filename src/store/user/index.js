444ce41985778a0598033f63a33692f5cc74b382
CristiAllan <cristiallan@gmail.com>
2019-11-17 13:38:44 -0300

    implement userModule

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,3 +1,42 @@
-const userModule = {}
+const userModule = {
+  namespaced: true,
+  state: {
+    id: '',
+    name: '',
+    email: ''
+  },
+
+  mutations: {
+    setId (state, id) {
+      state.id = id
+    },
+    setName (state, name) {
+      state.name = name
+    },
+    setEmail (state, email) {
+      state.email = email
+    }
+  },
+
+  actions: {
+    getData ({ commit }) {
+      const url = 'https://jsonplaceholder.typicode.com/users/1'
+
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          commit('setId', json.id)
+          commit('setName', json.name)
+          commit('setEmail', json.email)
+        })
+        .catch(error => console.log(error))
+    }
+
+  },
+
+  getters: {
+    upperName: state => state.name.toUpperCase()
+  }
+}
 
 export default userModule
